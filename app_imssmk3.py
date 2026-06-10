7e51ac674c1e336824f64cf384fa08850a0d6b88
arydaffadaffisukses-ISOConsultant <arydaffadaffisukses@gmail.com>
2026-06-10 17:50:39 +0700

    Update page title for the dashboard

--- app_imssmk3.py
+++ app_imssmk3.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 # ==========================================
 # 1. KONFIGURASI HALAMAN UTAMA DASHBOARD
 # ==========================================
-st.set_page_config(page_title="SRIS IMS & SMK3 Dashboard", layout="wide", page_icon="🛡️")
+st.set_page_config(page_title="SRIS Engine Dashboard", layout="wide", page_icon="🛡️")
 
 # Desain Tema Premium Corporate Executive (HSE Green & Deep Slate)
 st.markdown("""
